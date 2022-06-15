c6fc88d3572838d2fb53214caf8e78917cff3b04
TomasKarbauskas <karbauskastomas@gmail.com>
2022-06-15 12:01:06 +0300

    practical exercise

---
+++ leson2.php
@@ -0,0 +1,71 @@
+<?php
+
+//declare(strict_types=1);
+//$variableToDebug = 15;
+////
+//var_dump($variableToDebug);
+//echo $variableToDebug;
+
+//
+//var_dump([
+//    'Hello',
+//    'Learning',
+//    'Amazing',
+//    'CodeAcademy',
+//    'Php',
+//    'Git'
+//]);
+//
+//$variable1 = false;
+//$variable2 = true;
+//$variable3 = '2022 year';
+//
+//var_dump((int)$variable1);
+//var_dump((int)$variable2);
+//var_dump((int)$variable3);
+//
+//$variable4 = 150;
+//$variable5 = [];
+//$variable6 = 'Year - 2022';
+//
+//var_dump((bool)$variable4);
+//var_dump((bool)$variable5);
+//var_dump((bool)$variable6);
+//
+//$variable7 = 145;
+//$variable8 = '145';
+//$variable9 = '150';
+//$variable10 = 150;
+//
+//var_dump($variable7 !== (int)$variable8);
+//var_dump($variable7 === (int)$variable8);
+//
+//var_dump($variable10 !== (int)$variable9);
+//var_dump($variable10 === (int)$variable9);
+
+
+//$v1 = 5;
+//$v2 = 6;
+//$v1--;
+//$v2++;
+//var_dump(143 * 444);
+//var_dump(567 / 12);
+//var_dump(578 % 55);
+//var_dump( 6 ** 5);
+//var_dump($v1);
+//var_dump($v2);
+
+//$var1 = '230';
+//$var2 = '45';
+//var_dump(56 > 77);
+//var_dump(190 <= 230);
+//var_dump(230 === (int)$var1);
+//var_dump(45 === (int)$var2);
+//
+//$variable = 10;
+
+
+var_dump (isset($variable));
+
+
+
