3b50d9b33ba3b9fde33339aeebd873377354cbcb
TomasKarbauskas <karbauskastomas@gmail.com>
2022-06-15 12:45:28 +0300

    practical exercise

--- leson1/leson1.php
+++ leson1/leson1.php
@@ -1,4 +1,22 @@
 <?php
 
-echo 'Hello';
-echo PHP_EOL;
+//echo 'Hello';
+//echo PHP_EOL;
+//
+//include_once 'file_to_load.php';
+//
+//die();
+//
+//echo 'You should not be seeing this line printed';
+
+$variableToDebug = 15;
+
+echo ($variableToDebug);
+var_dump($variableToDebug);
+
+
+
+
+
+
+
