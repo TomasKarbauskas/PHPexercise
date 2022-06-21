9b7ea2e8b0b4131a474f7a87bc4dd278151026a1
TomasKarbauskas <karbauskastomas@gmail.com>
2022-06-21 10:50:44 +0300

    lesson4 homework

--- lesson4/lesson4.php
+++ lesson4/lesson4.php
@@ -1,3 +1,26 @@
 <?php
-echo 'test';
-var_dump('test');
+
+//function dividesBy5 (int $numberToDivide): int {
+//    return $numberToDivide / 5;
+//};
+//echo (dividesBy5(15));
+
+
+//$array = [1, 2, 3, 4, 5];
+//function arrayPrinter ($array) {
+//    foreach ($array as $showValue) {
+//        echo $showValue . "\n";
+//    }
+//};
+//echo (arrayPrinter(['some text', 'another text']));
+//echo (arrayPrinter($array));
+
+
+//function stringEnhancer (string $word, $enhancer) {
+//    return $enhancer . $word . $enhancer;
+//}
+//echo stringEnhancer('Yellow submarine', '##');
+
+
+
+
