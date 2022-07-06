c5e4ca3ab51ce322117a50ae5c5b4014992f4cb2
TomasKarbauskas <karbauskastomas@gmail.com>
2022-07-06 11:31:39 +0300

    work complete

--- lesson9Date/advancedExercises.php
+++ lesson9Date/advancedExercises.php
@@ -42,3 +42,75 @@
 //    return $productsPurchasedPresentYear;
 //}
 //print_r(exercise1());
+
+
+//function exercise2(bool $showOnlyDays): void
+//{
+//    $products = [
+//        [
+//            'name' => 'Wine glass',
+//            'last_purchase' => '2022 Jan 15 18:34:12',
+//        ],
+//        [
+//            'name' => 'Bread knife',
+//            'last_purchase' => '2020 Mar 15 23:14:00',
+//        ],
+//        [
+//            'name' => 'Blue chair',
+//            'last_purchase' => '2019 Dec 12 15:00:12',
+//        ],
+//        [
+//            'name' => 'Cutting board',
+//            'last_purchase' => '2022 Feb 1 03:15:01',
+//        ],
+//    ];
+
+    /*
+    Išspausdinkite paskutinių pirkimų santrauką. Jeigu $showOnlyDays yra true, tuomet rodykite tik dienas.
+    Funkcijos kvietimas: exercise11(false)
+    Rezultatas:
+    Last purchased:
+    Wine glass 0 years 1 month 23 days ago
+    ...
+    Funkcijos kvietimas: exercise11(true)
+    Rezultatas:
+    Last purchased:
+    Wine glass 51 days ago
+    ...
+    */
+
+//    $presentDate = new DateTime();
+//
+//    if ($showOnlyDays === false) {
+//        $outputFormat = 'Wine glass %y years %m month %d days ago';
+//    } else if ($showOnlyDays === true){
+//            $outputFormat = 'Wine glass %d days ago';
+//    }
+//
+//    foreach ($products as $key => $value){
+//        $dateObject = date_create_from_format('Y M j H:i:s', $value['last_purchase']);
+//        $dateToCompare = $presentDate->diff($dateObject);
+//        print_r($value['name'].$dateToCompare->format($outputFormat).PHP_EOL);
+//    }
+//}
+//exercise2(false);
+
+
+//    function exercise3(int $numberOfCycles): void
+//    {
+//        /*
+//        Išspausdinkite kiek laiko trunka prasukti tuščią ciklą nurodytą kiekį kartų ($numberOfCycles).
+//        Trukmę apvalinkite iki milisekundžių.
+//        Pridėkite parametrui $numberOfCycles numatytąją reikšmę 1000000.
+//        */
+//
+//        $i = 0;
+//        $start = hrtime(true);
+//
+//        while ($i < $numberOfCycles) {
+//            $i++;
+//        }
+//        echo (hrtime((true) - $start)/1000000).'ms';
+//
+//    }
+//    exercise3(1000000);
