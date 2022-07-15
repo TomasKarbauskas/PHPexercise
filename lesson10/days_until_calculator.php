9ba81dbe311f25cd372311c391bc8522f10d3066
TomasKarbauskas <karbauskastomas@gmail.com>
2022-07-15 21:18:52 +0300

    work incomplete readline function does not work, readline resolve pending

---
+++ lesson10/days_until_calculator.php
@@ -0,0 +1,19 @@
+<?php
+
+echo 'enter event and date'.PHP_EOL;
+
+$event = readline('Event name:');
+$eventDate = readline('Event date (YYYY-MM-DD):');
+
+//$date = date_create($eventDate);
+//
+//$presentDate = new DateTime();
+//
+//    $dateToCompare = $presentDate->diff($date);
+//    $output = ($dateToCompare->format('%a'));
+//
+//    echo 'event'. $event. 'is'. $output. 'days away';
+
+
+
+
