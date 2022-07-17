86476e9d4b2cb6cd5d139b89145feaa5ed962ec3
TomasKarbauskas <karbauskastomas@gmail.com>
2022-07-17 20:37:10 +0300

    work complete

--- lesson10/days_until_calculator.php
+++ lesson10/days_until_calculator.php
@@ -1,18 +1,32 @@
 <?php
 
 echo 'enter event and date'.PHP_EOL;
+//simulated readline function
+if(!function_exists("readline")) {
+    function readline($prompt = null){
+        if($prompt){
+            echo $prompt;
+        }
+        $fp = fopen("php://stdin","r");
+        $line = rtrim(fgets($fp, 1024));
+        return $line;
+    }
+}
 
 $event = readline('Event name:');
 $eventDate = readline('Event date (YYYY-MM-DD):');
 
-//$date = date_create($eventDate);
-//
-//$presentDate = new DateTime();
-//
-//    $dateToCompare = $presentDate->diff($date);
-//    $output = ($dateToCompare->format('%a'));
-//
-//    echo 'event'. $event. 'is'. $output. 'days away';
+$date = new DateTime($eventDate);
+
+$presentDate = new DateTime();
+
+    $dateToCompare = $presentDate->diff($date);
+    $output = ($dateToCompare->format('%a'));
+
+
+    echo 'Event '.' \''.$event.'\''. ' is '. $output. ' days away ';
+
+
 
 
 
